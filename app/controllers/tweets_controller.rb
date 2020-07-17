a5b3dc1f1ff3c8b32c8954d54824350caf1d8b1c
828app <828app@gmail.com>
2020-07-17 14:52:51 +0900

    beforeaction編集

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,5 @@
 class TweetsController < ApplicationController
-  before_action :require_login,except: :index
+  before_action :require_login,except: [:index, :login]
 
   def tweet
       @tweets = Tweet.order("updated_at DESC")
